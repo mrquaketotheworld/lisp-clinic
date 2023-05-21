b3c444779276c359acbc190989a5c6e72f88690b
mrquaketotheworld <newrushiflyer@gmail.com>
2023-05-21 19:39:25 +0300

    Add ring router

--- src/core.clj
+++ src/core.clj
@@ -2,17 +2,24 @@
   (:gen-class)
   (:require [ring.adapter.jetty :refer [run-jetty]]
             [ring.middleware.reload :refer [wrap-reload]]
-            [db.init-tables :as init-tables]))
+            [db.init-tables :as init-tables]
+            [reitit.ring :as ring]))
 
 (defn handler [request]
+  (println (:path-params request))
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body "Hello World, again!"})
 
 (def wrapped-handler (-> #'handler wrap-reload))
 
+(def app
+  (ring/ring-handler
+    (ring/router
+      [["/" wrapped-handler]
+       ["/hello/:id" wrapped-handler]])))
+
 (defn -main [& args]
   (if (= (first args) "init-tables")
     (init-tables/-main)
-    (run-jetty wrapped-handler {:port 3000
-                                :join? false})))
+    (run-jetty app {:port 3000 :join? false})))
