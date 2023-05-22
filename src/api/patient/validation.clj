2f63119b33110b57697d7884f93a3e15baf4269f
mrquaketotheworld <newrushiflyer@gmail.com>
2023-05-22 13:06:24 +0300

    Refactor add handler validation

--- src/api/patient/validation.clj
+++ src/api/patient/validation.clj
@@ -11,19 +11,17 @@
 (def CITY-MAXLENGTH 128)
 (def STREET-MAXLENGTH 128)
 (def MID-LENGTH 12)
-(spec/def :patient/first-name #(<= (count %) NAME-MAXLENGTH))
-(spec/def :patient/last-name #(<= (count %) NAME-MAXLENGTH))
-(spec/def :patient/gender #(or (= % "Male") (= % "Female")))
-(spec/def :patient/birth valid-ddmmyyyy?)
-(spec/def :patient/city #(<= (count %) CITY-MAXLENGTH))
-(spec/def :patient/street #(<= (count %) STREET-MAXLENGTH))
-(spec/def :patient/house int?)
-(spec/def :patient/mid #(= (count %) MID-LENGTH))
-(spec/def :patient/patient (spec/keys :req-un
-                                      [:patient/first-name :patient/last-name :patient/gender
-                                       :patient/birth :patient/city :patient/street :patient/house
-                                       :patient/mid]))
+(spec/def ::first-name #(<= (count %) NAME-MAXLENGTH))
+(spec/def ::last-name #(<= (count %) NAME-MAXLENGTH))
+(spec/def ::gender #(or (= % "Male") (= % "Female")))
+(spec/def ::birth valid-ddmmyyyy?)
+(spec/def ::city #(<= (count %) CITY-MAXLENGTH))
+(spec/def ::street #(<= (count %) STREET-MAXLENGTH))
+(spec/def ::house int?)
+(spec/def ::mid #(= (count %) MID-LENGTH))
+(spec/def ::patient (spec/keys :req-un [::first-name ::last-name ::gender ::birth ::city ::street
+                                        ::house ::mid]))
 
 (defn is-add-handler-valid? [patient]
-  (spec/valid? :patient/patient patient))
+  (spec/valid? ::patient patient))
 
