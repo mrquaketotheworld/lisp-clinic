70299f9c4f9f341fdf326c0add123d5d49f13f4b
mrquaketotheworld <newrushiflyer@gmail.com>
2023-05-22 19:19:55 +0300

    Add model patient

---
+++ src/db/models/patient.clj
@@ -0,0 +1,27 @@
+(ns db.models.patient
+  (:require [db.credentials :refer [db]]
+            [next.jdbc :as jdbc]
+            [next.jdbc.sql :as sql])
+  (:import [java.time LocalDate]))
+
+(defn add [{:keys [first-name last-name gender birth-day birth-month birth-year city street house
+                   mid]}]
+  (jdbc/with-transaction [db-con db]
+    (sql/insert! db-con :patient {:first_name first-name
+                                  :last_name last-name
+                                  :gender_type gender
+                                  :birth (LocalDate/of birth-year birth-month birth-day)
+                                  :mid mid})
+
+    (let [address-id (jdbc/execute-one! db-con ["SELECT id FROM address
+                                                WHERE city = ? and street = ? and house = ?"
+                                                city street house])]
+      (if address-id
+        (sql/insert! db-con :patient_address {:patient_mid mid
+                                              :address_id (:address/id address-id)})
+        (let [created-address-id (:address/id (sql/insert! db-con :address
+                                                           {:city city :street street
+                                                            :house house} {:return-keys ["id"]}))]
+          (sql/insert! db-con :patient_address {:patient_mid mid
+                                                :address_id created-address-id}))))))
+
