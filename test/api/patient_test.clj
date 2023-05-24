46e3ceb04a18c6037f3f5e909c5aecfe63072e6c
mrquaketotheworld <newrushiflyer@gmail.com>
2023-05-24 22:18:01 +0300

    Add patient edit test, fix misc

--- test/api/patient_test.clj
+++ test/api/patient_test.clj
@@ -1,7 +1,6 @@
 (ns api.patient-test
   (:require [clojure.test :refer [deftest is testing use-fixtures]]
             [next.jdbc :as jdbc]
-            [next.jdbc.sql :as sql]
             [ring.mock.request :as mock]
             [core]
             [config :as config-src-dir]
@@ -16,6 +15,12 @@
 
 (defn mock-request-patient-add [body]
   (mock-request :post "/api/patient/add" body))
+
+(defn mock-request-patient-edit [body]
+  (mock-request :post "/api/patient/edit" body))
+
+(defn patient-main-info [patient]
+  (dissoc patient :birth :updated_at :created_at))
 
 (defn db-fixture [test-run]
   (config-src-dir/set-config! db-test) ; make global TEST configuration
@@ -38,13 +43,13 @@
                                  :street "Big apple       "
                                  :house 20
                                  :mid mid})
-      (let [patient (patient/get-by-mid mid)]
-        (is (= (dissoc patient :birth :updated_at :created_at) {:first_name "Homer"
-                                                                :last_name "Simpson"
-                                                                :gender_type "Male"
-                                                                :mid mid}))
-        (is (= (.toString (:birth patient)) "1965-12-25")))
-      (let [patient-address (address/get-by-mid mid)]
+      (let [patient (patient/get-by-mid mid)
+            patient-address (address/get-by-mid mid)]
+        (is (= (patient-main-info patient) {:first_name "Homer"
+                                            :last_name "Simpson"
+                                            :gender_type "Male"
+                                            :mid mid}))
+        (is (= (.toString (:birth patient)) "1965-12-25"))
         (is (= patient-address {:city "New York" :street "Big Apple" :house 20})))))
 
   (testing "Patient gets existing address"
@@ -70,9 +75,7 @@
                                  :house house
                                  :mid same-address-second-mid})
       (let [patient-address (address/get-by-mid same-address-second-mid)
-            addresses (sql/find-by-keys db-test :address {:city city
-                                                          :street street
-                                                          :house house})]
+            addresses (address/match-address city street house)]
         (is (and (= patient-address {:city city
                                      :street street
                                      :house house}) (= 1 (count addresses)))))))
@@ -135,7 +138,48 @@
             patient (patient/get-by-mid mid)]
         (is (and (= "{\"success\":true}" (:body response)) (nil? patient)))))))
 
-#_(deftest patient-edit
-    (println 'RUN-PATIENT-EDIT)
-    (testing "Context of the test assertions"
-      (is (= assertion-values))))
+(deftest patient-edit
+  (println 'RUN-PATIENT-EDIT)
+  (testing "Edit was edited"
+    (let [mid "243283439393" new-first-name "Slim" new-last-name "Shady" gender "Male"
+          new-city "Detroit" new-street "Snow" new-house 25]
+      (mock-request-patient-add {:first-name "Marshall"
+                                 :last-name "Mather"
+                                 :gender "Male"
+                                 :birth-day 19
+                                 :birth-month 7
+                                 :birth-year 1970
+                                 :city "Compton"
+                                 :street "Smith"
+                                 :house 24
+                                 :mid mid})
+      (mock-request-patient-edit {:first-name new-first-name
+                                  :last-name new-last-name
+                                  :gender gender
+                                  :birth-day 19
+                                  :birth-month 7
+                                  :birth-year 1970
+                                  :city new-city
+                                  :street new-street
+                                  :house new-house
+                                  :mid mid})
+      (let [patient (patient/get-by-mid mid)
+            patient-address (address/get-by-mid mid)]
+        (is (= (patient-main-info patient) {:first_name new-first-name
+                                            :last_name new-last-name
+                                            :gender_type gender
+                                            :mid mid}))
+        (is (= (.toString (:birth patient)) "1970-07-19"))
+        (is (= patient-address {:city new-city :street new-street :house new-house})))))
+  (testing "Patient doesn't exist"
+    (let [response (mock-request-patient-edit {:first-name "John"
+                                               :last-name "Chan"
+                                               :gender "Male"
+                                               :birth-day 16
+                                               :birth-month 8
+                                               :birth-year 1981
+                                               :city "Tokio"
+                                               :street "Alex Yao"
+                                               :house 2
+                                               :mid "423838383838"})]
+      (is (= "{\"error\":\"Patient doesn't exist\"}" (:body response))))))
