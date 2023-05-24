68ea07cb6faa0a7fe23a8e8b468f67d0004476dc
mrquaketotheworld <newrushiflyer@gmail.com>
2023-05-24 20:19:26 +0300

    Add get-by-mid func in address model

--- src/db/models/address.clj
+++ src/db/models/address.clj
@@ -1,10 +1,19 @@
 (ns db.models.address
-  (:require [next.jdbc.sql :as sql]))
+  (:require [next.jdbc.sql :as sql]
+            [next.jdbc :as jdbc]
+            [next.jdbc.result-set :as rs]
+            [config :refer [db-config]]))
 
 (defn match-address [db-con city street house]
   (:address/id (first (sql/find-by-keys db-con :address {:city city :street street :house house}))))
 
 (defn add [db-con city street house]
   (:address/id (sql/insert! db-con :address
-                {:city city :street street
-                 :house house} {:return-keys ["id"]})))
+                            {:city city :street street
+                             :house house} {:return-keys ["id"]})))
+
+(defn get-by-mid [mid]
+  (jdbc/execute-one! db-config ["SELECT city, street, house FROM address
+                                WHERE id = (SELECT address_id FROM
+                                patient_address WHERE patient_mid = ?)" mid]
+                     {:builder-fn rs/as-unqualified-lower-maps}))
