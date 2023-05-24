172761f3bee23a8e1ede7e10cc0396c9b51ddcab
mrquaketotheworld <newrushiflyer@gmail.com>
2023-05-24 18:56:26 +0300

    Refactor tests

--- test/api/patient_test.clj
+++ test/api/patient_test.clj
@@ -8,6 +8,13 @@
             [config-test :refer [db-test]]
             [db.init-tables :as init-tables]))
 
+(defn mock-request [request-type url body]
+  (core/wrapped-app (-> (mock/request request-type url)
+                        (mock/json-body body))))
+
+(defn mock-request-patient-add [body]
+  (mock-request :post "/api/patient/add" body))
+
 (defn db-fixture [test-run]
   (config-src-dir/set-config! db-test) ; make global TEST configuration
   (jdbc/execute-one! db-test ["DROP TABLE IF EXISTS address, gender, patient,
@@ -19,17 +26,16 @@
 (deftest patient-add
   (println 'RUN-PATIENT-ADD)
   (testing "Patient was saved formatted"
-    (core/wrapped-app (-> (mock/request :post "/api/patient/add")
-                          (mock/json-body {:first-name "Homer"
-                                           :last-name "Simpson"
-                                           :gender "male"
-                                           :birth-day 25
-                                           :birth-month 12
-                                           :birth-year 1965
-                                           :city "        New YorK"
-                                           :street "Big apple"
-                                           :house 20
-                                           :mid "123426782326"})))
+    (mock-request-patient-add {:first-name "Homer"
+                               :last-name "Simpson"
+                               :gender "male"
+                               :birth-day 25
+                               :birth-month 12
+                               :birth-year 1965
+                               :city "        New YorK"
+                               :street "Big apple"
+                               :house 20
+                               :mid "123426782326"})
     (let [patient (jdbc/execute-one! db-test ["SELECT first_name, last_name, gender_type, mid,
                                               birth
                                               FROM patient"]
@@ -49,17 +55,16 @@
                               :street "Big Apple"
                               :house 20}))))
   (testing "Next patient has address like previous one and address unique"
-    (core/wrapped-app (-> (mock/request :post "/api/patient/add")
-                          (mock/json-body {:first-name "Bart"
-                                           :last-name "Simpson"
-                                           :gender "male"
-                                           :birth-day 20
-                                           :birth-month 11
-                                           :birth-year 1989
-                                           :city "New york"
-                                           :street "big apple"
-                                           :house 20
-                                           :mid "123426782327"})))
+    (mock-request-patient-add {:first-name "Bart"
+                               :last-name "Simpson"
+                               :gender "male"
+                               :birth-day 20
+                               :birth-month 11
+                               :birth-year 1989
+                               :city "New york"
+                               :street "big apple"
+                               :house 20
+                               :mid "123426782327"})
     (let [patient-address (jdbc/execute-one! db-test ["SELECT city, street, house FROM address
                                               WHERE id = (SELECT address_id FROM patient_address
                                                            WHERE patient_mid = ?)" "123426782327"]
@@ -70,48 +75,48 @@
                                    :street "Big Apple"
                                    :house 20}) (= 1 (count addresses))))))
   (testing "Patient already exists"
-    (let [response (core/wrapped-app (-> (mock/request :post "/api/patient/add")
-                                         (mock/json-body {:first-name "Bart"
-                                                          :last-name "Simpson"
-                                                          :gender "male"
-                                                          :birth-day 20
-                                                          :birth-month 11
-                                                          :birth-year 1989
-                                                          :city "New york"
-                                                          :street "big apple"
-                                                          :house 20
-                                                          :mid "123426782327"})))]
+    (let [response (mock-request-patient-add {:first-name "Bart"
+                                              :last-name "Simpson"
+                                              :gender "male"
+                                              :birth-day 20
+                                              :birth-month 11
+                                              :birth-year 1989
+                                              :city "New york"
+                                              :street "big apple"
+                                              :house 20
+                                              :mid "123426782327"})]
       (is (= "{\"error\":\"Patient already exists\"}" (:body response)))))
   (testing "Patient was saved"
-    (let [response (core/wrapped-app (-> (mock/request :post "/api/patient/add")
-                                         (mock/json-body {:first-name "Liza"
-                                                          :last-name "Simpson"
-                                                          :gender "Female"
-                                                          :birth-day 30
-                                                          :birth-month 10
-                                                          :birth-year 1994
-                                                          :city "New york"
-                                                          :street "big apple"
-                                                          :house 20
-                                                          :mid "123426782328"})))]
+    (let [response (mock-request-patient-add {:first-name "Liza"
+                                              :last-name "Simpson"
+                                              :gender "Female"
+                                              :birth-day 30
+                                              :birth-month 10
+                                              :birth-year 1994
+                                              :city "New york"
+                                              :street "big apple"
+                                              :house 20
+                                              :mid "123426782328"})]
       (is (= "{\"success\":true}" (:body response))))))
 
 (deftest patient-delete
   (println 'RUN-PATIENT-DELETE)
   (testing "Patient was deleted"
-    (core/wrapped-app (-> (mock/request :post "/api/patient/add")
-                          (mock/json-body {:first-name "Marge"
-                                           :last-name "Simpson"
-                                           :gender "Female"
-                                           :birth-day 19
-                                           :birth-month 7
-                                           :birth-year 1970
-                                           :city "New york"
-                                           :street "big apple"
-                                           :house 20
-                                           :mid "123426782329"})))
-    (let [response (core/wrapped-app (-> (mock/request :delete "/api/patient/delete")
-                                         (mock/json-body {:mid "123426782329"})))
+    (mock-request-patient-add {:first-name "Marge"
+                               :last-name "Simpson"
+                               :gender "Female"
+                               :birth-day 19
+                               :birth-month 7
+                               :birth-year 1970
+                               :city "New york"
+                               :street "big apple"
+                               :house 20
+                               :mid "123426782329"})
+    (let [response (mock-request :delete "/api/patient/delete" {:mid "123426782329"})
           patient (jdbc/execute-one! db-test ["SELECT * FROM patient"])]
       (is (and (= "{\"success\":true}" (:body response)) (nil? patient))))))
 
+#_(deftest patient-edit
+    (println 'RUN-PATIENT-EDIT)
+    (testing "Context of the test assertions"
+      (is (= assertion-values))))
