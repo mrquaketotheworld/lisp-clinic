556f31e6b3fe3cdaebcddd0fa65c88de5248f8eb
mrquaketotheworld <newrushiflyer@gmail.com>
2023-05-24 18:20:55 +0300

    Refactor address, patient models

--- src/db/models/address.clj
+++ src/db/models/address.clj
@@ -2,9 +2,9 @@
   (:require [next.jdbc.sql :as sql]))
 
 (defn match-address [db-con city street house]
-  (first (sql/find-by-keys db-con :address {:city city :street street :house house})))
+  (:address/id (first (sql/find-by-keys db-con :address {:city city :street street :house house}))))
 
 (defn add [db-con city street house]
-  (sql/insert! db-con :address
-               {:city city :street street
-                :house house} {:return-keys ["id"]}))
+  (:address/id (sql/insert! db-con :address
+                {:city city :street street
+                 :house house} {:return-keys ["id"]})))
--- src/db/models/patient.clj
+++ src/db/models/patient.clj
@@ -2,11 +2,19 @@
   (:require [config :refer [db-config]]
             [next.jdbc :as jdbc]
             [next.jdbc.sql :as sql]
-            [db.models.address :as address])
+            [db.models.address :as address]
+            [db.models.patient-address :as patient-address])
   (:import [java.time LocalDate]))
 
 (defn does-exist? [mid]
   (boolean (first (sql/find-by-keys db-config :patient {:mid mid}))))
+
+(defn assign-address [db-con mid city street house]
+  (let [address-id (address/match-address db-con city street house)]
+    (if address-id
+      (patient-address/add db-con mid address-id)
+      (let [created-address-id (address/add db-con city street house)]
+        (patient-address/add db-con mid created-address-id)))))
 
 (defn add [{:keys [first-name last-name gender birth-day birth-month birth-year city street house
                    mid]}]
@@ -16,14 +24,7 @@
                                   :gender_type gender
                                   :birth (LocalDate/of birth-year birth-month birth-day)
                                   :mid mid})
-
-    (let [address-id (address/match-address db-con city street house)]
-      (if address-id
-        (sql/insert! db-con :patient_address {:patient_mid mid
-                                              :address_id (:address/id address-id)})
-        (let [created-address-id (:address/id (address/add db-con city street house))]
-          (sql/insert! db-con :patient_address {:patient_mid mid
-                                                :address_id created-address-id}))))))
+    (assign-address db-con mid city street house)))
 
 (defn delete [mid]
   (sql/delete! db-config :patient {:mid mid}))
@@ -37,11 +38,5 @@
                                   :birth (LocalDate/of birth-year birth-month birth-day)}
                  {:mid mid})
     (sql/delete! db-con :patient_address {:patient_mid mid})
-    (let [address-id (address/match-address db-con city street house)]
-      (if address-id
-        (sql/update! db-con :patient_address {:address_id (:address/id address-id)}
-                     {:patient_mid mid})
-        (let [created-address-id (:address/id (address/add db-con city street house))]
-          (sql/insert! db-con :patient_address {:patient_mid mid
-                                                :address_id created-address-id}))))))
+    (assign-address db-con mid city street house)))
 
