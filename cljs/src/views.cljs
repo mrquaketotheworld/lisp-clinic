58190861d27bdc4a72edeb0d0775b0f9d1bb64d3
mrquaketotheworld <newrushiflyer@gmail.com>
2023-05-31 14:38:36 +0300

    Add modal, table basic components

--- cljs/src/views.cljs
+++ cljs/src/views.cljs
@@ -1,19 +1,67 @@
-(ns views)
+(ns views) ; TOOD remove redundant CSS
+
+(defn input [label input-name placeholder classes]
+  [:div.column
+   [:label.label label]
+   [:div.control.is-expanded.has-icons-left
+    [:input.input {:type "text" :name input-name
+                   :placeholder (str placeholder)}]
+    [:div.icon.is-small.is-left
+     [:i {:class classes}]]]])
+
+(defn select-gender-simple []
+  [:div.column.gender-simple
+   [:label.label "Gender"]
+   [:div.control.has-icons-left
+    [:div.select
+     [:select {:name "gender"}
+      [:option {:value "Male"} "Male"]
+      [:option {:value "Female"} "Female"]]]
+    [:div.icon.is-small.is-left
+     [:i.fa-regular.fa-person-circle-question]]]])
+
+(defn modal []
+  [:div.modal.is-active
+   [:div.modal-background]
+   [:div.modal-card
+    [:header.modal-card-head
+     [:p.modal-card-title "title"]
+     [:button.delete]]
+    [:section.modal-card-body
+     [:div.columns
+      [input "First Name" "first-name" "Homer" "fa-solid fa-address-card"]
+      [input "Last Name" "last-name" "Simpson" "fa-solid fa-address-card"]]
+     [:div.columns
+      [select-gender-simple]
+      [:div.column
+       [:label.label "Birth"]
+       [:input.input {:type "date" :min "1900-01-01" :max (-> (js/Date.)
+                                                              .toISOString
+                                                              (.split "T")
+                                                              first)}]]
+      [input "Medical Insurance ID" "mid" "342929393329" "fa-solid fa-file-medical"]]
+     [:div.columns
+      [input "City" "city" "New York" "fa-solid fa-globe"]
+      [input "Street" "street" "Park Avenue" "fa-solid fa-road"]
+      [input "House" "house" "332" "fa-solid fa-house"]]]
+
+    [:footer.modal-card-foot
+     [:button.button.is-success "Saved"]
+     [:button.button "Cancel"]]]])
 
 (defn select-age [select-name default-option]
   [:div.control.has-icons-left
    [:div.select
-    [:select {:name select-name}
-     [:option {:value "0"} default-option]
-     (map (fn [i] [:option {:value i :key i} i]) (range 1 100))]]
+    [:select {:name select-name :default-value default-option}
+     (map (fn [i] [:option {:value i :key i} i]) (range 101))]]
    [:div.icon.is-small.is-left
     [:i.fa-solid.fa-list-ol]]])
 
 (defn select-age-bottom []
-  [select-age "age-bottom" "From"])
+  [select-age "age-bottom" "0"])
 
 (defn select-age-top []
-  [select-age "age-top" "Till"])
+  [select-age "age-top" "100"])
 
 (defn select-city []
   [:div.control.has-icons-left
@@ -23,19 +71,12 @@
    [:div.icon.is-small.is-left
     [:i.fa-solid.fa-globe]]])
 
-(defn input-text [label]
-  [:div.field
-   [:label.label {:for label} label]
-   [:div.control
-    [:input.input {:type "text" :id label :name (.toLowerCase label)
-                   :placeholder (str "Enter " label)}]]])
-
 (defn select-gender []
-  [:div
-   "Gender"
+  [:div.field
+   [:label.label "Gender"]
    [:div.control.has-icons-left
     [:div.select
-     [:select {:name "Gender"}
+     [:select {:name "gender"}
       [:option {:value ""} "All"]
       [:option {:value "Male"} "Male"]
       [:option {:value "Female"} "Female"]]]
@@ -48,26 +89,46 @@
     [:p.level-item
      [:img.logo {:src "images/lispclinic.png"}]]]
    [:div.columns.box
+    [:div.column [select-gender]]
     [:div.column
-     [select-gender]]
-    [:div.column
-
-     [:div "Age" [:div.field.is-grouped
-                  [select-age-bottom]
-                  [select-age-top]]]]
-
-    [:div.column "City" [select-city]]
-
+     [:div.field [:label.label "Age"] [:div.field.is-grouped
+                                       [select-age-bottom]
+                                       [select-age-top]]]]
+    [:div.column [:div.field [:label.label "City"] [select-city]]]
     [:div.column.is-two-fifths.is-align-self-flex-end
      [:div.field.is-grouped
-      [:div.control.is-expanded
+      [:div.control.is-expanded.has-icons-left
        [:input.input {:type "search" :name "search"
-                      :placeholder (str "John Doe, 381293...")}]]
+                      :placeholder (str "John Doe, 381293...")}]
+       [:div.icon.is-small.is-left
+        [:i.fa-sharp.fa-solid.fa-magnifying-glass]]]
       [:div.control
        [:button.button.is-primary "Search"]]]]]])
 
-(defn container []
-  [header])
+(defn table []
+  [:div.columns.box.mt-4
+   [:div.column
+    [:table.table
+     [:thead
+      [:tr
+       [:th "#"]
+       [:th "Name"]
+       [:th "Gender"]
+       [:th "Birth"]
+       [:th "Address"]
+       [:th "MID"]]]
+
+     [:tbody
+      [:tr
+       [:th 1]
+       [:td "Homer Simpson"]
+       [:td "Male"]
+       [:td "05-25-1956"]
+       [:td "New York, Green street, 35"]
+       [:td "34324jj32322"]]]]]])
 
 (defn app []
-  [container])
+  [:<>
+   [header]
+   [table]
+   [modal]])
