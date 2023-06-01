2f40774b920cbb04d1e0ad3c0608186020c77e0f
mrquaketotheworld <newrushiflyer@gmail.com>
2023-06-01 18:27:03 +0300

    Refactor add/edit validation

--- clj/src/utils/validation/patient/new_form.clj
+++ clj/src/utils/validation/patient/new_form.clj
@@ -1,11 +1,9 @@
 (ns utils.validation.patient.new-form
   (:require [clojure.spec.alpha :as spec]
+            [utils.validation.error :as error :refer [NAME-MAXLENGTH CITY-MAXLENGTH
+                                                      MID-LENGTH STREET-MAXLENGTH]]
             [utils.format.time :as time]))
 
-(def NAME-MAXLENGTH 128)
-(def CITY-MAXLENGTH 128)
-(def STREET-MAXLENGTH 128)
-(def MID-LENGTH 12)
 (spec/def ::firstname #(<= (count %) NAME-MAXLENGTH))
 (spec/def ::lastname #(<= (count %) NAME-MAXLENGTH))
 (spec/def ::gender #(or (= % "Male") (= % "Female")))
@@ -17,28 +15,8 @@
 (spec/def ::patient (spec/keys :req-un [::firstname ::lastname ::gender ::birth ::city ::street
                                         ::house ::mid]))
 
-(defn max-length [max]
-  (str "Max length is: " max))
-
-(def errors
-  {:firstname (max-length NAME-MAXLENGTH)
-   :lastname (max-length NAME-MAXLENGTH)
-   :gender "Can be Male or Female"
-   :birth "Format should be yyyy-mm-dd"
-   :city (max-length CITY-MAXLENGTH)
-   :street (max-length STREET-MAXLENGTH)
-   :house "Should be a number"
-   :mid (str "Length should be: " MID-LENGTH)})
-
 (defn validate [patient]
-  (let [explained-data (::spec/problems (spec/explain-data ::patient patient))
-        error (reduce (fn [acc problem]
-                        (let [key-error (first (:path problem))]
-                          (if key-error
-                            (assoc acc key-error (key-error errors))
-                            (assoc acc :some-of-keys-are-not-valid (keys (:val problem)))))) {}
-                      explained-data)]
-    (when (seq error) error)))
+  (error/validate ::patient patient))
 
 
 
