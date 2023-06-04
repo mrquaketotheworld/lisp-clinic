a19c49e3c4b7190c923e42f4e149da22fcf92aa4
mrquaketotheworld <newrushiflyer@gmail.com>
2023-06-04 16:16:29 +0300

    Fix temp error

--- cljs/src/events.cljs
+++ cljs/src/events.cljs
@@ -154,7 +154,7 @@
          [:dispatch [:modal-active? true]]]}))
 
 (rf/reg-event-fx
- :dit-patient-form
+ :edit-patient-form
  check-spec-interceptor
  (fn [{:keys [db]} [_ mid]]
    {:db (assoc-in db [:patient] (first (filter #(= mid (:mid %)) (:patients db))))
