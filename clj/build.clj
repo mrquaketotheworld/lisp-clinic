dcf4ed4e8e369c528065713c551491646dadc050
mrquaketotheworld <newrushiflyer@gmail.com>
2023-06-04 22:04:21 +0300

    Fix build.clj

--- clj/build.clj
+++ clj/build.clj
@@ -1,11 +1,9 @@
 (ns build
   (:require [clojure.tools.build.api :as b]))
 
-(def lib 'lisp-clinic)
-(def version (format "0.0.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
-(def uber-file (format "target/%s-%s-standalone.jar" (name lib) version))
+(def uber-file "target/lisp-clinic.jar")
 
 (defn clean [_]
   (b/delete {:path "target"}))
